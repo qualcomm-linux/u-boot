bcdc6f78d81555274d91ff6bbce0e66febf1effa
Aswin Murugan <aswin.murugan@oss.qualcomm.com>
2026-05-11 12:40:52 +0530

    mach-snapdragon: Add early EL2 hypervisor switch support
    Add support for exiting Gunyah hypervisor and switching to EL2 during
    early boot. This is required for platforms that boot U-Boot in a
    hypervisor guest environment where U-Boot needs to run at EL2 to
    enable other hypervisors like KVM.
    
    The switch is performed via TrustZone SMC call before EL register
    configuration in start.S. The implementation:
    
    - Checks current exception level and only executes at EL1
    - Uses TrustZone SMC to exit Gunyah and transition to EL2
    - Integrates with existing boot0.h workaround infrastructure
    
    A new Kconfig option CONFIG_QCOM_EL2_GUNYAH_EXIT_SUPPORT enables this
    functionality.
    
    Signed-off-by: Aswin Murugan <aswin.murugan@oss.qualcomm.com>

--- arch/arm/mach-snapdragon/include/mach/boot0.h
+++ arch/arm/mach-snapdragon/include/mach/boot0.h
@@ -1,9 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
+
 #if defined(CONFIG_SPL_BUILD)
 	b	reset
 #else
 #if defined(CONFIG_BOOT0_MSM8916_PSCI_WORKAROUND)
 #include "msm8916_boot0.h"
+#elif defined(CONFIG_QCOM_EL2_GUNYAH_EXIT_SUPPORT)
+#include "gunyah_exit_boot0.h"
 #else
 	b	reset
 #endif
