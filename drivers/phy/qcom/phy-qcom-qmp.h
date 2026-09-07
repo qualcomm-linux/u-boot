6e5ac353d1128b2ca4eee95bf64b3614a5be68ee
Senthilkumar <snlakshm@qti.qualcomm.com>
2026-09-07 23:46:15 +0530

    phy: qcom: qmp-usb: add sa535m (echo) v9 register layout support
    Add the QMP USB3 uni-phy v9 register layout (offsets struct, regs_layout
    array, and 4 new qserdes/pcs header files) plus sa535m's own serdes/tx/
    rx/pcs/pcs_usb init tables and of_match entry (qcom,echo-qmp-usb3-uni-phy).
    
    The same-Wildcat-family sdx75 PHY was considered as a reuse candidate,
    but its cfg struct uses the v6 register layout -- a different layout
    version than echo's v9, confirmed directly from the kernel's own
    phy-qcom-qmp-usb.c. This is therefore a new register-table port, not a
    DTS-only compatible-string retarget.
    
    Signed-off-by: Senthil <snlakshm@qti.qualcomm.com>

--- drivers/phy/qcom/phy-qcom-qmp.h
+++ drivers/phy/qcom/phy-qcom-qmp.h
@@ -36,6 +36,9 @@
 #include "phy-qcom-qmp-qserdes-txrx-v8.h"
 #include "phy-qcom-qmp-qserdes-lalb-v8.h"
 
+#include "phy-qcom-qmp-qserdes-com-v9.h"
+#include "phy-qcom-qmp-qserdes-txrx-v9.h"
+
 #include "phy-qcom-qmp-qserdes-pll.h"
 
 #include "phy-qcom-qmp-pcs-v2.h"
@@ -61,6 +64,8 @@
 #include "phy-qcom-qmp-pcs-v8.h"
 
 #include "phy-qcom-qmp-pcs-v8_50.h"
+
+#include "phy-qcom-qmp-pcs-v9.h"
 
 /* QPHY_SW_RESET bit */
 #define SW_RESET				BIT(0)
